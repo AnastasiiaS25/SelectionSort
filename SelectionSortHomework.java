9ad86cbd7dbc31805d7dd7efe87d894a879d9aba
Anastasiia <150678939+AnastasiiaS25@users.noreply.github.com>
2024-02-02 15:50:42 +0100

    Add files via upload

---
+++ SelectionSortHomework.java
@@ -0,0 +1,21 @@
+package pl.edu.agh.asd2.sort;
+
+public class SelectionSortHomework {
+
+    public static int[] selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minId = i;
+            for (int j = i+1; j < array.length; j++){
+                if (array[j] < min) {
+                    min = array[j];
+                    minId = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = min;
+            array[minId] = temp;
+        }
+        return null;
+    }
+}
